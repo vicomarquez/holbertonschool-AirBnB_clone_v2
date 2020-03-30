1b3c90ffa1087f24977217cbda1818aa3685274f
justinmajetich <justinmajetich@gmail.com>
2020-03-30 22:48:15 +0000

    Begin implementing delete method

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -20,11 +20,16 @@ class FileStorage:
     __file_path = "file.json"
     __objects = {}
 
-    def all(self):
+    def all(self, cls=None):
         """returns a dictionary
         Return:
             returns a dictionary of __object
         """
+        if cls in 
+        # check for cls arg
+        # varify class exists
+        # construct class-specific dict
+        # if none, return all
         return self.__objects
 
     def new(self, obj):
@@ -55,3 +60,11 @@ class FileStorage:
                     self.__objects[key] = value
         except FileNotFoundError:
             pass
+
+    def delete(self, obj=None):
+        """delete obj from __objects if present
+        """
+        if obj:
+            # format key from obj
+            key = "{}.{}".format(type(obj).__name__, obj.id)
+            del self.__objects[key]
