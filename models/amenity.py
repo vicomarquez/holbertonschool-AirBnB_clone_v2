eea25156e96df6cf7319d89aab087656ba3367c7
Jeremy Bedolla <jjbedolla@gmail.com>
2020-04-01 18:02:20 -0400

    updated for db mapping

--- models/amenity.py
+++ models/amenity.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
 """This is the amenity class"""
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+import sqlalchemy
+from sqlalchemy import Column, String
 
 
-class Amenity(BaseModel):
+class Amenity(BaseModel, Base):
     """This is the class for Amenity
     Attributes:
         name: input name
     """
-    name = ""
+    __tablename__ = 'amenities'
+    name = Column(String(128), cascade='all, delete', nullable=False)
