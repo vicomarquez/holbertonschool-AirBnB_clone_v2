59ae19f090e79438c664d64ef0b6704efdc4bd1f
Jeremy Bedolla <jjbedolla@gmail.com>
2020-04-01 18:05:36 -0400

    removed commented out code

--- models/review.py
+++ models/review.py
@@ -17,7 +17,3 @@ class Review(BaseModel, Base):
     place_id = Column(String(60), ForeignKey('places.id'), nullable=False)
     user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
     text = Column(String(1024), nullable=False)
-
-    # def __init__(self, *args, **kwargs):
-    #    """ initialize Review """
-    #    super().__init__(*args, **kwargs)
