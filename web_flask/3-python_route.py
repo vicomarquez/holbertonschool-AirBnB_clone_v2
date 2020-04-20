63a43806f5e4249e0c1128b58e96a4450e632b7d
justinmajetich <justinmajetich@gmail.com>
2020-04-20 22:14:17 +0000

    Write script to handle optional path variables

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+"""Start web application with two routings
+"""
+
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello():
+    """Return string when route queried
+    """
+    strict_slashes = False
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb')
+def hbnb():
+    """Return string when route queried
+    """
+    strict_slashes = False
+    return 'HBNB'
+
+
+@app.route('/c/<text>')
+def c_is_fun(text):
+    """Return reformatted text
+    """
+    strict_slashes = False
+    return 'C ' + text.replace(' ', '_')
+
+
+@app.route('/python/')
+@app.route('/python/<text>')
+def python_with_text(text=None):
+    """Reformat text based on optional variable
+    """
+    strict_slashes = False
+
+    if text:
+        return 'Python ' + text.replace(' ', '_')
+    else:
+        return 'Python is cool'
+
+
+if __name__ == '__main__':
+    app.url_map.strict_slashes = False
+    app.run(host='0.0.0.0', port=5000)
