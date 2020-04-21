8a27acb191819fd92cc6d6da98bc071a130c07c1
justinmajetich <justinmajetich@gmail.com>
2020-04-21 19:11:27 +0000

    Sort State objects by name before rendering template

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -14,11 +14,13 @@ def states_list():
     """
     path = '7-states_list.html'
     states = storage.all(State)
-    return render_template(path, states=states)
+    # sort State object alphabetically by name
+    sorted_states = sorted(states.values(), key=lambda state: state.name)
+    return render_template(path, sorted_states=sorted_states)
 
 
 @app.teardown_appcontext
-def app_teardown(param=None):
+def app_teardown(arg=None):
     """Clean-up session
     """
     storage.close()
