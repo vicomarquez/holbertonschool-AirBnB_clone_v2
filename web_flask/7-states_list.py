0d57224dc203d1919730791019cc3f92cae1e935
vicomarquez <vicomarquezmuniz@gmail.com>
2022-12-04 20:11:56 -0300

    fix task

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -2,24 +2,23 @@
 """displays a text variable"""
 
 
+from models import storage
 from flask import Flask
 from flask import render_template
-from models import storage
-from models.state import State
-
 
 app = Flask(__name__)
+
+
+@app.route("/states_list", strict_slashes=False)
+def stat_lst():
+    states = storage.all("State")
+    return render_template("7-states_list.html", states=states)
 
 
 @app.teardown_appcontext
 def teardown(self):
     storage.close()
 
-
-@app.route("/states_list", strict_slashes=False)
-def list():
-    states = storage.all('State')
-    return render_template("7-states_list.html", states=states)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
