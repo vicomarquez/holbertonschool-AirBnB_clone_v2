b573dcd99c0e744fdddfe56443d7aeceb608349d
vicomarquez <vicomarquezmuniz@gmail.com>
2022-12-04 13:51:17 -0300

    task 8

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""displays a text variable"""
+
+
+from flask import Flask
+from flask import render_template
+from models import storage
+
+
+app = Flask(__name__)
+
+
+@app.route("/states_list", strict_slashes=False)
+def list():
+    return render_template("7-states_list.html", states=storage.all(State))
+
+
+@app.teardown_appcontext
+def close():
+    storage.close()
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
